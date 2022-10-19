c7f1df7df536a1b6fd30e813dfa9fcd94a095d7e
s_du <sungju29@g.skku.edu>
2022-10-19 14:12:32 +0900

    changed list into variable, added 1 video file

--- detector.py
+++ detector.py
@@ -2,10 +2,12 @@ import cv2
 import torch
 import matplotlib.pyplot as plt
 import os
+import timeit
+
 
 model_path = './vehicle_detector/best.pt'
 model = torch.hub.load(os.getcwd(), 'custom', source='local', path = model_path, force_reload=True)
-video = './video/test2_Trim.mp4'
+video = './video/test1.mp4'
 print(torch.cuda.is_available())
 
 cap = cv2.VideoCapture(video)
@@ -13,56 +15,58 @@ if cap.isOpened():
     while True:
         ret, img = cap.read()
         if ret:
+            start_t = timeit.default_timer()
             results = model(img)
-            print("----- results -----")
-            print(results)
+            # print("----- results -----")
+            # print(results)
 
             # Number of cars in current frame
             num_of_cars = len(results.pandas().xyxy[0]['xmin'])
 
             # Lists of results from yolo directly
-            xmin_list = []
-            ymin_list = []
-            xmax_list = []
-            ymax_list = []
-            confidence_list = []
+            # xmin_list = []
+            # ymin_list = []
+            # xmax_list = []
+            # ymax_list = []
+            # confidence_list = []
 
             # Lists of width, height, centroid calculation
-            width_list = []
-            height_list = []
-            centroid_list = []
+            # width_list = []
+            # height_list = []
+            # centroid_list = []
 
-            # Add into lists & draw bbox
+            # # Add into lists & draw bbox
             for iter in range(num_of_cars):
-                print("----- ", iter, "st car ------")
-                xmin_list.append(results.pandas().xyxy[0]['xmin'][iter])
-                ymin_list.append(results.pandas().xyxy[0]['ymin'][iter])
-                xmax_list.append(results.pandas().xyxy[0]['xmax'][iter])
-                ymax_list.append(results.pandas().xyxy[0]['ymax'][iter])
-                confidence_list.append(results.pandas().xyxy[0]['confidence'][iter])
+                # print("----- ", iter, "st car ------")
+                xmin = (results.pandas().xyxy[0]['xmin'][iter])
+                ymin = (results.pandas().xyxy[0]['ymin'][iter])
+                xmax = (results.pandas().xyxy[0]['xmax'][iter])
+                ymax = (results.pandas().xyxy[0]['ymax'][iter])
+                confidence = (results.pandas().xyxy[0]['confidence'][iter])
 
-                print("xmin : ", results.pandas().xyxy[0]['xmin'][iter])
-                print("ymin : ", results.pandas().xyxy[0]['ymin'][iter])
-                print("xmax : ", results.pandas().xyxy[0]['xmax'][iter])
-                print("ymax : ", results.pandas().xyxy[0]['ymax'][iter])
-                print("confidence : ", results.pandas().xyxy[0]['confidence'][iter])
+                # print("xmin : ", xmin)
+                # print("ymin : ", ymin)
+                # print("xmax : ", xmax)
+                # print("ymax : ", ymax)
+                # print("confidence : ", confidence)
 
-                width_list.append(abs(int(xmax_list[iter]) - int(xmin_list[iter])))
-                height_list.append(abs(int(ymax_list[iter]) - int(ymin_list[iter])))
-                centroid_list.append(((int(xmin_list[iter]) + int(xmax_list[iter]))/2, (int(ymin_list[iter]) + int(ymax_list[iter]))/2))
+                # width_list.append(abs(int(xmax) - int(xmin)))
+                # height_list.append(abs(int(ymax) - int(ymin)))
+                # centroid_list.append(((int(xmin) + int(xmax))/2, (int(ymin) + int(ymax))/2))
 
-                # Check
-                # print(type(confidence_list[iter]))
-
-                # cv2.putText(img, str(confidence_list[iter]), (int(xmin_list[iter]), int(ymin_list[iter])), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3)
-                # cv2.rectangle(img, (int(xmin_list[iter]), int(ymin_list[iter])), (int(xmax_list[iter]), int(ymax_list[iter])), (0, 0, 255), 3)                
+                cv2.putText(img, str(confidence), (int(xmin), int(ymin)), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 5)
+                cv2.rectangle(img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 0, 255), 5)                
 
             # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             
             # plt.imshow(imgRGB)
             # plt.show()
-
+            end_t = timeit.default_timer()
+            fps = int(1./(end_t - start_t))
+            print("FPS : ", fps)
             cv2.imshow('Result', img)
+            cv2.waitKey(1)
+            
         
         else:
             break
