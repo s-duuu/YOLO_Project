7c59e88aa51a420ed47cf3b1bbbb53abd978fece
SeongJu KIm <sungju29@g.skku.edu>
2022-10-23 21:18:46 +0900

    잡다한 것들 수정

--- detector.py
+++ detector.py
@@ -1,3 +1,4 @@
+from turtle import left
 import cv2
 import torch
 import matplotlib.pyplot as plt
@@ -36,6 +37,12 @@ if cap.isOpened():
             # centroid_list = []
 
             # # Add into lists & draw bbox
+            cnt = 0
+            max_val = -1
+            max_x_min = 0
+            max_x_max = 0
+            max_y_min = 0
+            max_y_max = 0
             for iter in range(num_of_cars):
                 # print("----- ", iter, "st car ------")
                 xmin = (results.pandas().xyxy[0]['xmin'][iter])
@@ -52,15 +59,28 @@ if cap.isOpened():
 
                 width = abs(xmax - xmin)
                 height = abs(ymax - ymin)
+
+                if width > max_val:
+                    max_val = width
+                    max_x_min = xmin
+                    max_x_max = xmax
+                    max_y_min = ymin
+                    max_y_max = ymax
+                
                 # width_list.append(abs(int(xmax) - int(xmin)))
                 # height_list.append(abs(int(ymax) - int(ymin)))
                 # centroid_list.append(((int(xmin) + int(xmax))/2, (int(ymin) + int(ymax))/2))
 
                 # cv2.putText(img, str(confidence), (int(xmin), int(ymin)), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 5)
                 # width, height print
-                cv2.putText(img, str(width), (int(xmin), int(ymin)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 5)
-                cv2.putText(img, str(height), (int(xmin), int(ymax)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 5)
-                cv2.rectangle(img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 0, 255), 5)                
+                # cv2.putText(img, str(width), (int(xmin), int(ymin)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 5)
+                # cv2.putText(img, str(height), (int(xmin), int(ymax)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 5)
+                # cv2.rectangle(img, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 0, 255), 5)                
+
+            left_top = (int(max_x_min), int(max_y_min))
+            right_bottom = (int(max_x_max), int(max_y_max))
+
+            cv2.rectangle(img, left_top, right_bottom, (255,0,0), 5)
 
             # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             
